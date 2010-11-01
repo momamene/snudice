0c38fc1c29e57b69a912037837b5ef6645541765
firia2000@hotmail.com <firia2000@hotmail.com@8c51ee0e-be6f-364a-dc61-9bb944e96777>
2010-11-01 01:53:54 +0000

    사운드 모듈에 BGM,효과음 볼륨 조절 함수 추가

--- SNUDiCE_Client/PlaySoundCore.cpp
+++ SNUDiCE_Client/PlaySoundCore.cpp
@@ -14,9 +14,10 @@ gPlaySoundCore *gPlaySoundCore::GetIF()
 
 gPlaySoundCore::gPlaySoundCore() 
 {
-	this->Volume = 100;
+	this->bgmVolume = 100;
 	this->bgmStream = NULL;	
 	this->bgmState = BGM_NOT_LOADED;
+	this->effectSoundVolume = 100;
 
 	FSOUND_Init(44100,32,0);
 	FSOUND_DSP_SetActive(FSOUND_DSP_GetFFTUnit(),TRUE);		
@@ -33,9 +34,9 @@ void gPlaySoundCore::StartBGM(const char* filename)
 {
 	this->bgmStream=FSOUND_Stream_Open(filename,FSOUND_LOOP_NORMAL, 0, 0);
 	if (this->bgmStream!=NULL) {
-		this->channel=FSOUND_Stream_Play(FSOUND_FREE, this->bgmStream);		
+		this->bgmChannel=FSOUND_Stream_Play(FSOUND_FREE, this->bgmStream);		
 		// 새로 생성되는 채널은 항상 최대 볼륨이므로 강제로 맞춰줘야 함
-		FSOUND_SetVolume(this->channel,this->Volume);		
+		FSOUND_SetVolume(this->bgmChannel,this->bgmVolume);		
 
 		this->bgmState = BGM_PLAYING;
 	}	
@@ -45,7 +46,7 @@ void gPlaySoundCore::PauseBGM()
 	if(this->bgmState!=BGM_PLAYING)
 		return;
 
-	FSOUND_SetPaused(channel,TRUE);
+	FSOUND_SetPaused(bgmChannel,TRUE);
 	this->bgmState = BGM_PAUSED;
 }
 void gPlaySoundCore::ResumeBGM()
@@ -53,7 +54,7 @@ void gPlaySoundCore::ResumeBGM()
 	if(this->bgmState!=BGM_PAUSED)
 		return;
 
-	FSOUND_SetPaused(channel,FALSE);
+	FSOUND_SetPaused(bgmChannel,FALSE);
 	this->bgmState = BGM_PLAYING;
 }
 void gPlaySoundCore::StopBGM()
@@ -71,7 +72,7 @@ void gPlaySoundCore::PlayEffectSound(const char* filename)
 {
 	FSOUND_STREAM *effectSoundStream = FSOUND_Stream_Open(filename,FSOUND_NORMAL,0,0);
 	int effectSoundChannel = FSOUND_Stream_Play(FSOUND_FREE,effectSoundStream);
-	FSOUND_SetVolume(effectSoundChannel,this->Volume);
+	FSOUND_SetVolume(effectSoundChannel,this->effectSoundVolume);
 	FSOUND_Stream_SetEndCallback(effectSoundStream,&gPlaySoundCore::disposeEffectSoundStream,NULL);
 }
 
@@ -82,4 +83,23 @@ signed char F_CALLBACKAPI gPlaySoundCore::disposeEffectSoundStream(FSOUND_STREAM
 	FSOUND_Stream_Close(stream);
 
 	return 0;
+}
+
+int gPlaySoundCore::getBGMVolume()
+{
+	return this->bgmVolume;
+}
+void gPlaySoundCore::setBGMVolume(int newBGMVolume)
+{
+	this->bgmVolume = newBGMVolume;
+	FSOUND_SetVolume(this->bgmChannel,this->bgmVolume);
+}
+int gPlaySoundCore::getEffectSoundVolume()
+{
+	return this->effectSoundVolume;
+}
+//effect sound 의 바뀐 음량은 다음 effect sound play 때부터 적용된다.
+void gPlaySoundCore::setEffectSoundVolume(int newEffectSoundVolume)
+{
+	this->effectSoundVolume = newEffectSoundVolume;
 }
--- SNUDiCE_Client/PlaySoundCore.h
+++ SNUDiCE_Client/PlaySoundCore.h
@@ -22,16 +22,24 @@ public:
 	gPlaySoundCore();
 
 	bool isBGMLoaded();
+
 	void StartBGM(const char* filename);
 	void PauseBGM();
 	void ResumeBGM();
 	void StopBGM();
-	void PlayEffectSound(const char* filename);	
+	void PlayEffectSound(const char* filename);
+
+	int getBGMVolume();
+	void setBGMVolume(int newBGMVolume);
+	int getEffectSoundVolume();
+	void setEffectSoundVolume(int newEffectSoundVolume);
 
 private:
 	FSOUND_STREAM *bgmStream;	
-	int channel,Volume;
+	int bgmChannel;
+	int bgmVolume;
 	SOUNDSTATE bgmState;
+	int effectSoundVolume;
 
 private:
 	static signed char F_CALLBACKAPI disposeEffectSoundStream(FSOUND_STREAM *stream,void *buff,int len,void *userdata);
