c9b332bf0aaa375c5961290424328806fc50cb95
firia2000@hotmail.com <firia2000@hotmail.com@8c51ee0e-be6f-364a-dc61-9bb944e96777>
2010-12-27 19:37:04 +0000

    prev, next 완료

--- snudiceWebpage/src/constant/Const.java
+++ snudiceWebpage/src/constant/Const.java
@@ -1,7 +1,6 @@
 package constant;
 
 public class Const {
-	public static int articlePerPage = 3; //게시판 한 페이지당 보여줄 글 수
-	public static int pageNumberPerPage = 3; //게시판 한 페이지당 보여줄 페이지번호 수
-
+	public static int articlePerPage = 10; //게시판 한 페이지당 보여줄 글 수
+	public static int pageNumberPerPage = 6; //게시판 한 페이지당 보여줄 페이지번호 수
 }
