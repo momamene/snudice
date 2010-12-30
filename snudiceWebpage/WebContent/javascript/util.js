f0e85cafce78ebfc6ce154e5b75d50e04c0d83ad
firia2000@hotmail.com <firia2000@hotmail.com@8c51ee0e-be6f-364a-dc61-9bb944e96777>
2010-12-30 12:24:12 +0000

    관리자 페이지 - 게시판 삭제 기능 추가

--- snudiceWebpage/WebContent/javascript/util.js
+++ snudiceWebpage/WebContent/javascript/util.js
@@ -35,3 +35,32 @@ function createRequest()
 	}	
 	return request;
 }
+
+//요청을 보낸다.
+function sendRequest(url,method,param,callback,async)
+{
+	var request = createRequest();
+	if(request==null)
+	{
+		alert("AJAX 요청 객체를 만들 수 없습니다.");
+		return;
+	}	
+	
+	request.open(method,url,async);
+	request.onreadystatechange = callback;
+	if(method.toLowerCase()=="post")
+		request.setRequestHeader("Content-type","application/x-www-form-urlencoded");	
+	request.send(param);
+}
+
+//요청이 완료되면 현재 페이지를 새로고침 한다.
+function refresh()
+{
+	if(request.readyState == 4) //작업이 완료되었을 때
+	{
+		if(request.status == 200)
+		{
+			location.reload(); //페이지 새로고침
+		}
+	}	
+}
