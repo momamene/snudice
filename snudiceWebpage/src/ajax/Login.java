2939620a78653e51e6698aa7dfe440e3079a96f2
firia2000@hotmail.com <firia2000@hotmail.com@8c51ee0e-be6f-364a-dc61-9bb944e96777>
2011-01-02 11:17:20 +0000

    로그인 기능 구현

---
+++ snudiceWebpage/src/ajax/Login.java
@@ -0,0 +1,64 @@
+package ajax;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import dbaccess.DB;
+
+/**
+ * Servlet implementation class Login
+ */
+@WebServlet("/Login")
+public class Login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Login() {
+        super();
+        // TODO Auto-generated constructor stub
+    }	
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String userId = request.getParameter("userId");
+		String password = request.getParameter("password");			
+		
+		ServletOutputStream os = response.getOutputStream();
+		
+		HttpSession session = request.getSession();	
+		String sessionUserId = (String)session.getAttribute("userId");
+		
+		//이미 로그인 된 경우
+		if(sessionUserId!=null && sessionUserId.compareTo(userId)==0)
+		{
+			os.print("alreadyLoggined");
+			return;
+		}
+		
+		DB db = DB.getInstance();
+		boolean isExistingUser = db.dbAccount.isValidUser(userId,password);
+		if(!isExistingUser)		
+			os.print("loginFail");		
+		else
+		{
+			//session에 userId를 속성으로 지정					
+			session.setAttribute("userId", userId);	
+			
+			//secure login	
+			request.login(userId, password);		
+			
+			os.print("loginOK");
+		}		
+	}
+}
