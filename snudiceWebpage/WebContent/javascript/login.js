5235321ffeacd9844c33dacb5450e6c73afe89b0
firia2000@hotmail.com <firia2000@hotmail.com@8c51ee0e-be6f-364a-dc61-9bb944e96777>
2011-01-22 12:00:09 +0000

    메인메뉴 디자인 완성

--- snudiceWebpage/WebContent/javascript/login.js
+++ snudiceWebpage/WebContent/javascript/login.js
@@ -37,14 +37,54 @@ function initLogin(){
 	
 	//메뉴	
 	var menu1 = document.getElementById("menu1");
-	menu1.onmouseover = function() {
+	menu1.onmouseover = function() {		
 		var menu1DropDown = document.getElementById("menu1DropDown");
 		menu1DropDown.className = "menuDropDownWrapper visible"; 
 	};
-	menu1.onmouseout = function() {
+	menu1.onmouseout = function() {		
 		var menu1DropDown = document.getElementById("menu1DropDown");
 		menu1DropDown.className = "menuDropDownWrapper invisible"; 
 	};
+	
+	var menu2 = document.getElementById("menu2");
+	menu2.onmouseover = function() {
+		var menu2DropDown = document.getElementById("menu2DropDown");
+		menu2DropDown.className = "menuDropDownWrapper visible"; 
+	};
+	menu2.onmouseout = function() {
+		var menu2DropDown = document.getElementById("menu2DropDown");
+		menu2DropDown.className = "menuDropDownWrapper invisible"; 
+	};
+	
+	var menu3 = document.getElementById("menu3");
+	menu3.onmouseover = function() {
+		var menu3DropDown = document.getElementById("menu3DropDown");
+		menu3DropDown.className = "menuDropDownWrapper visible"; 
+	};
+	menu3.onmouseout = function() {
+		var menu3DropDown = document.getElementById("menu3DropDown");
+		menu3DropDown.className = "menuDropDownWrapper invisible"; 
+	};
+	
+	var menu4 = document.getElementById("menu4");
+	menu4.onmouseover = function() {
+		var menu4DropDown = document.getElementById("menu4DropDown");
+		menu4DropDown.className = "menuDropDownWrapper visible"; 
+	};
+	menu4.onmouseout = function() {
+		var menu4DropDown = document.getElementById("menu4DropDown");
+		menu4DropDown.className = "menuDropDownWrapper invisible"; 
+	};
+	
+	var menu5 = document.getElementById("menu5");
+	menu5.onmouseover = function() {
+		var menu5DropDown = document.getElementById("menu5DropDown");
+		menu5DropDown.className = "menuDropDownWrapper visible"; 
+	};
+	menu5.onmouseout = function() {
+		var menu5DropDown = document.getElementById("menu5DropDown");
+		menu5DropDown.className = "menuDropDownWrapper invisible"; 
+	};	
 }
 
 ////////////////////ajax 요청을 하는 event handler/////////////////////
